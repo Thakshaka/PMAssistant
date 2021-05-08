c01283f6f35c7bf7c6684a920d6bd4eeba6e0d9b
Thakshaka <76155498+Thakshaka@users.noreply.github.com>
2021-05-08 16:45:32 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -2,7 +2,7 @@
 # ©️2021 TeLe TiPs All Rights Reserved
 
 class Presets(object):
-    WELCOME_TEXT = "Hello, <b>{}</b>\n<code>I am a PM Assistant Bot created by TeLe TiPs\nSend your message to my master👇🏻</code> "
+    WELCOME_TEXT = "Hello, <b>{}</b>\n<code>I am a PM Assistant Bot of {OWNER_NAME}!\n\n{ok} created by TeLe TiPs\nSend your message to my master👇🏻</code> "
     USER_DETAILS = "<b>PM FROM:</b>\nName: {} {}\nId: {}\nUname: @{}\nScam: {}\nRestricted: {}\nStatus: {}\nDc Id: {}"
     PM_TXT_ATT = "<b>Message from:</b> {}\n<b>Name:</b> {}\n\n{}"
     PM_MED_ATT = "<b>Message from:</b> {} \n<b>Name:</b> {}"
