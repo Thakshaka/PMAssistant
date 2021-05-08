5c8be1b7b8fb2114f6bfd1915a5707bd26800927
Thakshaka <76155498+Thakshaka@users.noreply.github.com>
2021-05-08 16:30:07 +0530

    Initial Commit

--- presets.py
+++ presets.py
@@ -1,5 +1,5 @@
 class Presets(object):
-    WELCOME_TEXT = "Hello.. <b>{}</b>\n<code>Type your query here..\nI'll respond to your query as earliest</code> 😉"
+    WELCOME_TEXT = "Hello, <b>{}</b>\n<code>I am a PM Assistant Bot created by TeLe TiPs\nSend your message to my master👇🏻</code> "
     USER_DETAILS = "<b>PM FROM:</b>\nName: {} {}\nId: {}\nUname: @{}\nScam: {}\nRestricted: {}\nStatus: {}\nDc Id: {}"
     PM_TXT_ATT = "<b>Message from:</b> {}\n<b>Name:</b> {}\n\n{}"
     PM_MED_ATT = "<b>Message from:</b> {} \n<b>Name:</b> {}"
