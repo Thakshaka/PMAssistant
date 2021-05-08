7663fd14c159a99dca498e35b383f0bd32d50e9e
Thakshaka <76155498+Thakshaka@users.noreply.github.com>
2021-05-08 16:58:52 +0530

    Initial Commit

--- presets.py
+++ presets.py
@@ -3,3 +3,6 @@ class Presets(object):
     USER_DETAILS = "<b>PM FROM:</b>\nName: {} {}\nId: {}\nUname: @{}\nScam: {}\nRestricted: {}\nStatus: {}\nDc Id: {}"
     PM_TXT_ATT = "<b>Message from:</b> {}\n<b>Name:</b> {}\n\n{}"
     PM_MED_ATT = "<b>Message from:</b> {} \n<b>Name:</b> {}"
+
+# Thakshaka Rathnayake - TeLe TiPs
+# ©️2021 TeLe TiPs All Rights Reserved
